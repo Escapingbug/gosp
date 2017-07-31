a8963ffe38e9e43c48ba2ece6f3acd2c267be0ee
Anciety <ding641880047@126.com>
2017-07-31 17:29:03 +0800

    new file:   lib/parser.go 	new file:   lib/parser_test.go
    original commit

---
+++ lib/parser_test.go
@@ -0,0 +1,48 @@
+package parser
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestParserInt(t *testing.T) {
+	type MyStruct struct {
+		Num uint64
+	}
+	f, ok := os.Create("tempStruct")
+	if ok != nil {
+		t.Error("unable to create struct, error is ", ok)
+	}
+	buf := []byte{0xde, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef}
+	f.Write(buf)
+	f.Close()
+
+	f, ok = os.Open("tempStruct")
+	if ok != nil {
+		t.Error("unable to open created struct, error is ", ok)
+	}
+	parsed, ok := ParseStructFromBinaryStream(f, reflect.TypeOf(MyStruct{}), LITTLE_ENDIAN)
+	if ok != nil {
+		t.Error("parse error ", ok)
+	}
+	val, assertComplete := parsed.(reflect.Value)
+    if !assertComplete {
+        t.Error("Value type assertion error")
+    }
+
+    realVal, assertComplete := val.Interface().(MyStruct)
+    if !assertComplete {
+        t.Error("real struct type assertion error")
+    }
+
+    realVal.Num = 1
+    if (realVal.Num != 1) {
+        t.Error("error")
+    }
+
+    ok = os.Remove("tempStruct")
+    if ok != nil {
+        t.Error("temp file delete error")
+    }
+}
